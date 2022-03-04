bdd3504a962125c4e2f9c1ec944ce4736d7d8976
Haris Wicaksono <hariswicaksono7@gmail.com>
2022-03-04 14:19:29 +0700

    update

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -18,69 +18,23 @@ class HomeView extends GetView<HomeController> {
           style: TextStyle(fontFamily: "Kanit", fontSize: 16),
         ),
         centerTitle: true,
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            size: 30.0,
-          ),
-          onPressed: () => _key.currentState?.openDrawer(),
-        ),
+        leading: Builder(builder: (BuildContext context) {
+          return GestureDetector(
+            onTap: () {
+              Scaffold.of(context).openDrawer();
+            },
+            child: Container(
+              child: Icon(
+                Icons.menu,
+                size: 30.0,
+              ),
+              padding: EdgeInsets.all(7.0),
+            ),
+          );
+        }),
       ),
       drawer: MainNav(),
       body: HariIniView(),
     );
-
-    // List<Tab> myTab = [
-    //   Tab(
-    //     child: Text(
-    //       "Hari Ini",
-    //       style: TextStyle(fontFamily: "Kanit", fontSize: 16),
-    //     ),
-    //   ),
-    //   Tab(
-    //     child: Text(
-    //       "Bulan",
-    //       style: TextStyle(fontFamily: "Kanit", fontSize: 16),
-    //     ),
-    //   )
-    // ];
-    // return MaterialApp(
-    //   home: DefaultTabController(
-    //       length: myTab.length,
-    //       child: Scaffold(
-    //           appBar: PreferredSize(
-    //               preferredSize: Size.fromHeight(75.0),
-    //               child: Container(
-    //                 margin: EdgeInsets.only(top: 10),
-    //                 child: AppBar(
-    // title: Text(
-    //   'Aktivitasku',
-    //   style: TextStyle(fontFamily: "Kanit", fontSize: 16),
-    //                   ),
-    //                   centerTitle: true,
-    //                   leading: IconButton(
-    //                       onPressed: () {
-    //                         MainNav();
-    //                       },
-    //                       icon: Icon(
-    //                         Icons.menu,
-    //                         size: 30.0,
-    //                       )),
-    //                   actions: [
-    //                     SizedBox(
-    //                         height: 100.0,
-    //                         width: 60.0,
-    //                         child: Image.asset('assets/icons/mi_filter.png'))
-    //                   ],
-    //                   // bottom: TabBar(
-    //                   //     indicatorColor: Colors.white,
-    //                   //     indicatorPadding:
-    //                   //         EdgeInsets.only(right: 50, left: 50),
-    //                   //     tabs: myTab),
-    //                 ),
-    //               )),
-    //           drawer: MainNav(),
-    //           body: HariIniView())),
-    // );
   }
 }
